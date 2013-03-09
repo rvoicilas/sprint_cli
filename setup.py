20ede6559793312231741a7a6b259bc99eec361e
Radu Voicilas <radu.voicilas@gmail.com>
2013-03-09 19:46:01 +0200

    Better setup.py

--- setup.py
+++ setup.py
@@ -1,13 +1,21 @@
 from distutils.core import setup
 
-import sprint_cli
+from sprint_cli import __version__
 
 setup(
     name='sprint_cli',
-    version=sprint_cli.__version__,
-    packages=['sprint_cli'],
-    description='Command line interface to sprint.ly',
-    scripts=['bin/sprint-cli'],
-    long_description='buuu, have to do',
+    version=__version__,
     author='Radu Voicilas',
-    author_email='radu.voicilas@gmail.com')
+    author_email='radu.voicilas@gmail.com',
+    license='MIT',
+    description='Command line interface to sprint.ly',
+    long_description=open('README.md').read(),
+    packages=['sprint_cli'],
+    scripts=['bin/sprint-cli'],
+    classifiers=[
+        'Development Status :: 0.1dev',
+        'Topic :: Utilities',
+        'Programming Language :: Python :: 2.7',
+        'License :: OSI Approved :: MIT License'
+    ],
+)
